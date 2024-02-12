a5930bcd6211fc990df47eb13103f5430835698e
M K Messmer <sonnysboy4@gmail.com>
2024-02-11 22:26:11 -0800

    i'll finish tomorrow

--- purescript-book/exercises/chapter3/test/MySolutions.purs
+++ purescript-book/exercises/chapter3/test/MySolutions.purs
@@ -1,5 +1,15 @@
 module Test.MySolutions where
 
 import Prelude
-
+import Data.AddressBook
+import Data.Maybe
+import Data.List (List(..), filter, head)
 -- Note to reader: Add your solutions to this file
+findEntryByStreet :: String -> AddressBook -> Maybe Entry
+findEntryByStreet street = head <<< filter filterEntry
+    where
+        filterEntry :: Entry -> Boolean
+        filterEntry x = x.address.street == street
+
+findEntryByStreet' :: String -> AddressBook -> Maybe Entry
+findEntryByStreet' street = head <<< filter (_.address.street >>> (==) street) -- pointfree
