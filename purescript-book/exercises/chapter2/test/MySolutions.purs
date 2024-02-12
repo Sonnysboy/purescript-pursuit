b147da4847f7c1b875a4532e5505f678efc56c8b
M K Messmer <sonnysboy4@gmail.com>
2024-02-11 22:08:14 -0800

    light

--- purescript-book/exercises/chapter2/test/MySolutions.purs
+++ purescript-book/exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,13 @@
 module Test.MySolutions where
 
 import Prelude
+
+import Data.Int (rem)
+import Data.Number (pi)
+import Data.Number (sqrt)
+
+diagonal w h = sqrt (w * w + h * h)
+
+circleArea r = pi * (r * r)
+
+leftoverCents amt = amt `rem` 100
